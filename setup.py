03f840565ef64587ddb7a8b4145d8df7fb0279a3
K <kanalbant@gmail.com>
2019-01-22 21:02:51 -0600

    Fix missing history.rst file error

--- setup.py
+++ setup.py
@@ -8,9 +8,6 @@ from setuptools import setup, find_packages
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 requirements = [ ]
 
 setup_requirements = ['pytest-runner', ]
@@ -35,7 +32,7 @@ setup(
     description="A toolbox to create adversarial attacks with PyTorch",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='oppel',
     name='oppel',
